6780954683fc0e0bc3dc5cac8003fc8b2216eb37
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-20 10:13:04 +0100

    creating droppable container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-    state = {
-      items: [{name:"item1",
-               category:"uncomplete",
-               bgcolor: "red"},
+  state = {
+    items: [{
+      name: "item1",
+      category: "uncomplete",
+      bgcolor: "red"
+    },
 
-              {name:"item2",
-               category:"complete",
-               bgcolor:"green"},
+    {
+      name: "item2",
+      category: "complete",
+      bgcolor: "green"
+    },
 
-              {name:"item3",
-               category:"complete",
-               bgcolor:"blue"}
-        ]}
+    {
+      name: "item3",
+      category: "complete",
+      bgcolor: "blue"
+    }
+    ]
+  }
   render() {
     const items = {
       uncomplete: [],
@@ -21,17 +28,27 @@ export default class App extends Component {
     }
     this.state.items.forEach((i) => {
       items[i.category].push(
-      <div key = {i.name}
-        onDragStart ={(e) => this.onDragStart(e, i.name)}
-    draggable
-    className = 'draggable'
-    style={{backgroundColor: i.bgcolor}}>{i.name}
-    </div>)
-  })
+        <div key={i.name}
+          onDragStart={(e) => this.onDragStart(e, i.name)}
+          draggable
+          className='draggable'
+          style={{ backgroundColor: i.bgcolor }}>{i.name}
+        </div>)
+    })
     return (
       <div className="container">
-
-      </div>
-    );
+        <div className="uncomplete"
+          onDragOver={(e) => this.onDragOver(e)}
+          onDrop={(e) => { this.onDrop(e, "uncomplete") }}>
+          <span className="item-header">uncomplete</span>
+          {items.uncomplete}
+        </div>
+        <div className='droppable'
+          onDragOver={(e) => this.onDragOver(e)}
+          onDrop={(e) => this.onDrop(e, "complete")}>
+          <span className="item-header">complete</span>
+          {items.complete}
+        </div>
+      </div>);
   }
 }
