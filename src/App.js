8234319d5c0ec92376be27a65ee2b3bcc51efd01
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-20 11:28:09 +0100

    items can be drag and drop

--- src/App.js
+++ src/App.js
@@ -1,54 +1,78 @@
 import React, { Component } from 'react';
+import './App.css';
 
-export default class App extends Component {
+export default class AppDragDropDemo extends Component {
   state = {
-    items: [{
-      name: "item1",
-      category: "uncomplete",
-      bgcolor: "red"
-    },
-
-    {
-      name: "item2",
-      category: "complete",
-      bgcolor: "green"
-    },
-
-    {
-      name: "item3",
-      category: "complete",
-      bgcolor: "blue"
-    }
+    items: [
+      { name: "item1", category: "uncomplete", bgcolor: "yellow" },
+      { name: "item2", category: "uncomplete", bgcolor: "pink" },
+      { name: "item3", category: "complete", bgcolor: "skyblue" }
     ]
   }
+
+  onDragStart = (ev, id) => {
+    ev.dataTransfer.setData("id", id);
+  }
+
+  onDragOver = (ev) => {
+    ev.preventDefault();
+  }
+
+  onDrop = (ev, cat) => {
+    let id = ev.dataTransfer.getData("id");
+
+    let items = this.state.items.filter((item) => {
+      if (item.name === id) {
+        item.category = cat;
+      }
+      return item;
+    });
+
+    this.setState({
+      ...this.state,
+      items
+    });
+  }
+  reset = () => {
+this.render()
+}
   render() {
-    const items = {
+    var items = {
       uncomplete: [],
       complete: []
     }
+
     this.state.items.forEach((i) => {
       items[i.category].push(
         <div key={i.name}
           onDragStart={(e) => this.onDragStart(e, i.name)}
           draggable
-          className='draggable'
-          style={{ backgroundColor: i.bgcolor }}>{i.name}
-        </div>)
-    })
+          className="draggable"
+          style={{ backgroundColor: i.bgcolor }}
+        >
+          {i.name}
+        </div>
+      );
+    });
+
     return (
-      <div className="container">
+      <div className="container-drag">
+
         <div className="uncomplete"
           onDragOver={(e) => this.onDragOver(e)}
           onDrop={(e) => { this.onDrop(e, "uncomplete") }}>
           <span className="item-header">uncomplete</span>
           {items.uncomplete}
         </div>
-        <div className='droppable'
+        <div className="droppable"
           onDragOver={(e) => this.onDragOver(e)}
           onDrop={(e) => this.onDrop(e, "complete")}>
           <span className="item-header">complete</span>
           {items.complete}
         </div>
-      </div>);
+
+
+      </div>
+    );
   }
 }
